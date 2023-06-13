969f81a668603fa0d42ab2f7af3c2ad88176c5fe
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-14 01:02:54 +0300

    api: add connection manager

--- api.py
+++ api.py
@@ -1,9 +1,35 @@
-from fastapi import FastAPI, WebSocket, Request
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, WebSocketException, Request
 from logging import getLogger
 
 app = FastAPI()
 log = getLogger("WAS")
 websocket = WebSocket
+
+
+class ConnMgr:
+    def __init__(self):
+        self.connected_clients: list[websocket] = []
+
+    async def accept(self, ws: websocket):
+        try:
+            await ws.accept()
+            self.connected_clients.append(ws)
+        except WebSocketException as e:
+            log.error(f"failed to accept websocket connection: {e}")
+
+    async def broadcast(self, ws: websocket, msg: str):
+        for client in self.connected_clients:
+            try:
+                await client.send_text(msg)
+            except WebSocketException as e:
+                log.error(f"failed to broadcast message: {e}")
+
+    def disconnect(self, ws: websocket):
+        self.connected_clients.remove(ws)
+
+
+connmgr = ConnMgr()
+
 
 @app.get("/")
 def read_root():
@@ -13,13 +39,16 @@ def read_root():
 async def post_config(request: Request):
     data = await request.json()
     log.info(str(data))
-    await websocket.send(data)
+    await connmgr.broadcast(websocket, data)
     return "Success"
 
 @app.websocket_route("/ws")
 async def websocket_endpoint(websocket: websocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        log.info(str(data))
-        await websocket.send_text(data)
+    await connmgr.accept(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            log.info(str(data))
+            await connmgr.broadcast(websocket, data)
+    except WebSocketDisconnect:
+        connmgr.disconnect(websocket)
