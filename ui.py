c26a1d3c17f7d30a1fe51a5521afe7c01325b990
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-14 04:14:35 +0300

    ui: fix selectbox defaults

--- ui.py
+++ ui.py
@@ -29,27 +29,31 @@ except:
 config["wifi_ssid"] = st.text_input("WiFi SSID", value=config["wifi_ssid"])
 config["wifi_password"] = st.text_input("WiFi Password", value=config["wifi_password"])
 
+speech_rec_mode_values = ('WIS', 'Multinet')
 config["speech_rec_mode"] = st.selectbox(
     "Willow Speech Recognition Mode",
-    ('WIS', 'Multinet'))
+    speech_rec_mode_values, speech_rec_mode_values.index(config["speech_rec_mode"]))
 
 if config["speech_rec_mode"] == "WIS":
     config["wis_url"] = st.text_input("Willow Inference Server URL", value=config["wis_url"])
 
+response_type_values = ('None', 'Chimes', 'TTS')
 config["response_type"] = st.selectbox(
     "Willow audio response type",
-    ('None', 'Chimes', 'TTS'))
+    response_type_values, response_type_values.index(config["response_type"]))
 
 if config["response_type"] == "TTS":
     config["wis_tts_url"] = st.text_input("Willow Inference Server TTS URL", value=config["wis_tts_url"])
 
+wake_word_values = ('Hi ESP', 'Alexa', 'Hi Lexin')
 config["wake_word"] = st.selectbox(
     "Willow Wake Word",
-    ('Hi ESP', 'Alexa', 'Hi Lexin'))
+    wake_word_values, wake_word_values.index(config["wake_word"]))
 
+command_endpoint_values = ('Home Assistant', 'openHAB', 'REST')
 config["command_endpoint"] = st.selectbox(
     "Willow Command Endpoint",
-    ('Home Assistant', 'openHAB', 'REST'))
+    command_endpoint_values, command_endpoint_values.index(config["command_endpoint"]))
 
 if config["command_endpoint"] == "Home Assistant":
     config["hass_host"] = st.text_input("Home Assistant Host", value=config["hass_host"])
@@ -63,9 +67,10 @@ config["speaker_volume"] = st.slider('Speaker Volume', 0, 100, value=config["spe
 
 config["lcd_brightness"] = st.slider('LCD Brightness', 0, 1023, value=config["lcd_brightness"])
 
+ntp_config_values = ('Host', 'DHCP')
 config["ntp_config"] = st.selectbox(
     "NTP Configuration",
-    ('Host', 'DHCP'))
+    ntp_config_values, ntp_config_values.index(config["ntp_config"]))
 
 if config["ntp_config"] == "Host":
     config["ntp_host"] = st.text_input("NTP Host",value=config["ntp_host"])
