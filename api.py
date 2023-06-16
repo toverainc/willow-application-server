d5b08fe79060aafce7a378635a79a30f327c32f0
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-16 17:31:41 +0300

    api: add Client class
    Add a new Client class to keep a list of connected clients so we can
    keep additional data like UserAgent.

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 import json
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, WebSocketException, Request
+from fastapi import FastAPI, Header, WebSocket, WebSocketDisconnect, WebSocketException, Request
 from fastapi.responses import JSONResponse
 from logging import getLogger
+from typing import Annotated
 from websockets.exceptions import ConnectionClosed
 
 app = FastAPI()
@@ -9,26 +10,32 @@ log = getLogger("WAS")
 websocket = WebSocket
 
 
+class Client:
+    def __init__(self, ua, ws):
+        self.ua = ua
+        self.ws = ws
+
+
 class ConnMgr:
     def __init__(self):
-        self.connected_clients: list[websocket] = []
+        self.connected_clients: list[Client] = []
 
-    async def accept(self, ws: websocket):
+    async def accept(self, client: Client):
         try:
-            await ws.accept()
-            self.connected_clients.append(ws)
+            await client.ws.accept()
+            self.connected_clients.append(client)
         except WebSocketException as e:
             log.error(f"failed to accept websocket connection: {e}")
 
     async def broadcast(self, ws: websocket, msg: str):
         for client in self.connected_clients:
             try:
-                await client.send_text(msg)
+                await client.ws.send_text(msg)
             except WebSocketException as e:
                 log.error(f"failed to broadcast message: {e}")
 
-    def disconnect(self, ws: websocket):
-        self.connected_clients.remove(ws)
+    def disconnect(self, client: Client):
+        self.connected_clients.remove(client)
 
 
 connmgr = ConnMgr()
@@ -76,9 +83,11 @@ async def post_config(request: Request):
     await connmgr.broadcast(websocket, msg)
     return "Success"
 
-@app.websocket_route("/ws")
-async def websocket_endpoint(websocket: websocket):
-    await connmgr.accept(websocket)
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: websocket, user_agent: Annotated[str | None, Header(convert_underscores=True)] = None):
+    client = Client(user_agent, websocket)
+
+    await connmgr.accept(client)
     try:
         while True:
             data = await websocket.receive_text()
@@ -87,9 +96,10 @@ async def websocket_endpoint(websocket: websocket):
             if "cmd" in msg:
                 if msg["cmd"] == "get_config":
                     await websocket.send_text(build_config_msg(get_config_ws()))
+
             else:
                 await connmgr.broadcast(websocket, data)
     except WebSocketDisconnect:
-        connmgr.disconnect(websocket)
+        connmgr.disconnect(client)
     except ConnectionClosed:
-        connmgr.disconnect(websocket)
+        connmgr.disconnect(client)
