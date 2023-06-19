6fec88b1d505d22f628df8a4ffd38712b7bb2b68
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-19 16:19:56 +0300

    ui/clients: display clients in table

--- pages/1_Clients.py
+++ pages/1_Clients.py
@@ -1,3 +1,5 @@
+import json
+import pandas as pd
 import streamlit as st
 
 from shared.was import get_devices
@@ -8,7 +10,7 @@ title = "Willow Application Server - Devices"
 st.set_page_config(page_title=title, layout = 'centered', initial_sidebar_state = 'auto')
 st.title(title)
 
-
 devices = get_devices()
-for device in devices:
-    st.write(device)
+df = pd.read_json(json.dumps(devices))
+
+st.table(df)
