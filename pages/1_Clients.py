eb6412db0ea33295156e182f40bb49cb85bfc8ad
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-19 14:17:12 +0300

    ui/clients: list connected clients

--- pages/1_Clients.py
+++ pages/1_Clients.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
+from shared.was import get_devices
+
+
 title = "Willow Application Server - Devices"
 
 st.set_page_config(page_title=title, layout = 'centered', initial_sidebar_state = 'auto')
 st.title(title)
+
+
+devices = get_devices()
+for device in devices:
+    st.write(device)
