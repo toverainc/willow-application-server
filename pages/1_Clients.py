293d8ce798cdfdddb47b29baf48eda71b9c43e12
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-19 18:21:10 +0300

    pages/clients: custom table with OTA button

--- pages/1_Clients.py
+++ pages/1_Clients.py
@@ -1,8 +1,9 @@
 import json
 import pandas as pd
+import requests
 import streamlit as st
 
-from shared.was import get_devices
+from shared.was import get_devices, ota
 
 
 title = "Willow Application Server - Devices"
@@ -11,6 +12,17 @@ st.set_page_config(page_title=title, layout = 'centered', initial_sidebar_state 
 st.title(title)
 
 devices = get_devices()
-df = pd.read_json(json.dumps(devices))
+cols = st.columns(len(devices[0]) + 1)
+fields = list(devices[0])
 
-st.table(df)
+for col, field in zip(cols, fields):
+    col.write(f"**{field}**")
+
+
+for idx, row in enumerate(devices):
+    hostname, ip, port, user_agent, actions = st.columns(len(devices[0]) + 1)
+    hostname.write(row['hostname'])
+    ip.write(row['ip'])
+    port.write(row['port'])
+    user_agent.write(row['user_agent'])
+    actions.button(key=idx, kwargs=dict(hostname=row['hostname']), label="OTA", on_click=ota, type="primary")
