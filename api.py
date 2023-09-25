2c3b5b13c0c8b15745c3131931f52d60574d2f2a
Kristian Kielhofner <kris@tovera.com>
2023-09-25 10:08:35 -0500

    Pydantic checking for OTA upgrade GET route

--- api.py
+++ api.py
@@ -420,19 +420,24 @@ async def api_get_config(config: GetConfig = Depends()):
         config = get_multinet()
         return JSONResponse(content=config)
 
+
+class GetOta(BaseModel):
+    version: str = Field (Query(..., description='OTA Version'))
+    platform: str = Field (Query(..., description='OTA Platform'))
+
 @app.get("/api/ota")
-async def api_get_ota(version: str, platform: str):
-    ota_file = f"{DIR_OTA}/{version}/{platform}.bin"
+async def api_get_ota(ota: GetOta = Depends()):
+    ota_file = f"{DIR_OTA}/{ota.version}/{ota.platform}.bin"
     if not is_safe_path(DIR_OTA, ota_file):
         return
     if not os.path.isfile(ota_file):
         releases = get_releases_willow()
         for release in releases:
-            if release["name"] == version:
+            if release["name"] == ota.version:
                 assets = release["assets"]
                 for asset in assets:
-                    if asset["platform"] == platform:
-                        Path(f"{DIR_OTA}/{version}").mkdir(parents=True, exist_ok=True)
+                    if asset["platform"] == ota.platform:
+                        Path(f"{DIR_OTA}/{ota.version}").mkdir(parents=True, exist_ok=True)
                         r = get(asset["browser_download_url"])
                         open(ota_file, 'wb').write(r.content)
 
