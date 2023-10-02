ef743c1e612a3003b59b965a2db61304ef40ef68
Kristian Kielhofner <kris@tovera.com>
2023-10-02 11:34:07 -0500

    Support getting Willow timezone information from config route and worker on startup

--- shared/was.py
+++ shared/was.py
@@ -10,12 +10,14 @@ DIR_ASSET = '/app/storage/asset'
 DIR_OTA = '/app/storage/ota'
 URL_WILLOW_RELEASES = 'https://worker.heywillow.io/api/release?format=was'
 URL_WILLOW_CONFIG = 'https://worker.heywillow.io/api/config'
+URL_WILLOW_TZ = 'https://worker.heywillow.io/api/asset?type=tz'
 
 STORAGE_USER_CLIENT_CONFIG = 'storage/user_client_config.json'
 STORAGE_USER_CONFIG = 'storage/user_config.json'
 STORAGE_USER_MULTINET = 'storage/user_multinet.json'
 STORAGE_USER_NVS = 'storage/user_nvs.json'
 STORAGE_USER_WAS = 'storage/user_was.json'
+STORAGE_TZ = 'storage/tz.json'
 
 def construct_url(host, port, tls=False, ws=False):
     if tls:
