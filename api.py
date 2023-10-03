toverainc/willow-application-server

commit 62a4a0112882c4ccf530fb0461a5b5a1175792bc

Add processing for Willow goodbye message to remove client from connection manager

api.py
@@ -646,6 +646,9 @@ async def websocket_endpoint(
                 if msg["cmd"] == "get_config":
                     await websocket.send_text(build_msg(get_config_ws(), "config"))
 
+            elif "goodbye" in msg:
+                connmgr.disconnect(websocket)
+
             elif "hello" in msg:
                 if "hostname" in msg["hello"]:
                     connmgr.update_client(websocket, "hostname", msg["hello"]["hostname"])

commit ab9327b3e2765b867ea8bb3ffeb9ccea3184d8ac

api: check if connection exists before deleting
Since we're now removing a client from ConnMgr on the goodbye message,
trying to remove it again on ConnectionClosed or WebSocketDisconnect
exceptions causes another exception:

[2023-10-03T11:35:26] ERROR:    Exception in ASGI application
Traceback (most recent call last):
  File "/app/api.py", line 657, in websocket_endpoint
    data = await websocket.receive_text()
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/usr/local/lib/python3.12/site-packages/starlette/websockets.py", line 113, in receive_text
    self._raise_on_disconnect(message)
  File "/usr/local/lib/python3.12/site-packages/starlette/websockets.py", line 105, in _raise_on_disconnect
    raise WebSocketDisconnect(message["code"])
starlette.websockets.WebSocketDisconnect: 1005

During handling of the above exception, another exception occurred:

Traceback (most recent call last):
  File "/usr/local/lib/python3.12/site-packages/uvicorn/protocols/websockets/websockets_impl.py", line 247, in run_asgi
    result = await self.app(self.scope, self.asgi_receive, self.asgi_send)
             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/usr/local/lib/python3.12/site-packages/uvicorn/middleware/proxy_headers.py", line 84, in __call__
    return await self.app(scope, receive, send)
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/usr/local/lib/python3.12/site-packages/fastapi/applications.py", line 292, in __call__
    await super().__call__(scope, receive, send)
  File "/usr/local/lib/python3.12/site-packages/starlette/applications.py", line 122, in __call__
    await self.middleware_stack(scope, receive, send)
  File "/usr/local/lib/python3.12/site-packages/starlette/middleware/errors.py", line 149, in __call__
    await self.app(scope, receive, send)
  File "/usr/local/lib/python3.12/site-packages/starlette/middleware/cors.py", line 75, in __call__
    await self.app(scope, receive, send)
  File "/usr/local/lib/python3.12/site-packages/starlette/middleware/exceptions.py", line 79, in __call__
    raise exc
  File "/usr/local/lib/python3.12/site-packages/starlette/middleware/exceptions.py", line 68, in __call__
    await self.app(scope, receive, sender)
  File "/usr/local/lib/python3.12/site-packages/fastapi/middleware/asyncexitstack.py", line 20, in __call__
    raise e
  File "/usr/local/lib/python3.12/site-packages/fastapi/middleware/asyncexitstack.py", line 17, in __call__
    await self.app(scope, receive, send)
  File "/usr/local/lib/python3.12/site-packages/starlette/routing.py", line 718, in __call__
    await route.handle(scope, receive, send)
  File "/usr/local/lib/python3.12/site-packages/starlette/routing.py", line 341, in handle
    await self.app(scope, receive, send)
  File "/usr/local/lib/python3.12/site-packages/starlette/routing.py", line 82, in app
    await func(session)
  File "/usr/local/lib/python3.12/site-packages/fastapi/routing.py", line 324, in app
    await dependant.call(**values)
  File "/app/api.py", line 692, in websocket_endpoint
    connmgr.disconnect(websocket)
  File "/app/api.py", line 124, in disconnect
    self.connected_clients.pop(ws)
KeyError: <starlette.websockets.WebSocket object at 0x7f75ddc43770>

Fixes: 62a4a0112882

api.py
@@ -121,7 +121,8 @@ class ConnMgr:
                 log.error(f"failed to broadcast message: {e}")
 
     def disconnect(self, ws: WebSocket):
-        self.connected_clients.pop(ws)
+        if ws in self.connected_clients:
+            self.connected_clients.pop(ws)
 
     def get_client_by_hostname(self, hostname):
         for k, v in self.connected_clients.items():