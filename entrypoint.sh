e5cbca5125dc88d84d53a6c9470214bd25ddeb4b
Kristian Kielhofner <kris@tovera.com>
2023-10-11 11:18:43 -0500

    Migrate legacy WAS client device config on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ FORWARDED_ALLOW_IPS=${FORWARDED_ALLOW_IPS:-127.0.0.1}
 
 set +a
 
+python /app/misc/migrate_devices.py
+
 uvicorn api:app --host 0.0.0.0 --port 8502 --log-config uvicorn-log-config.json \
     --log-level "$LOG_LEVEL" --loop uvloop --timeout-graceful-shutdown 5 \
     --no-server-header
