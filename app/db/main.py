e654b7dd512a2faec3cda14efff4799647579862
Stijn Tintel <stijn@linux-ipv6.be>
2023-12-06 23:58:04 +0200

    db: migrate user_config.json to db on startup
    As we already have a pydantic model for the user config, let's leverage
    it here so we can spot potential issues early.

--- app/db/main.py
+++ app/db/main.py
@@ -1,9 +1,11 @@
 from logging import getLogger
 
-from sqlmodel import SQLModel, create_engine
+from sqlalchemy.exc import IntegrityError
+from sqlmodel import SQLModel, Session, create_engine
 
 from app.const import DB_URL
-from app.db.models import WillowConfigTable
+from app.db.models import WillowConfigTable, WillowConfigType
+from app.internal.config import WillowConfig
 
 
 log = getLogger("WAS")
@@ -14,3 +16,25 @@ engine = create_engine(DB_URL, echo=True, connect_args=connect_args)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+
+def migrate_user_config(config):
+    config = WillowConfig.parse_obj(config)
+    log.debug(f"config: {config}")
+
+    with Session(engine) as session:
+        for k, v in iter(config):
+            db_config = WillowConfigTable(
+                config_type=WillowConfigType.config,
+                config_name=k,
+            )
+            if v is not None:
+                db_config.config_value = str(v)
+            session.add(db_config)
+
+        try:
+            session.commit()
+        except IntegrityError as e:
+            # TODO avoid users thinking something is wrong here
+            log.warning(e)
+            session.rollback()
