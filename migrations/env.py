65c7d8596e6d1d2669fe416b2a3a67b5e4761a4c
Stijn Tintel <stijn@linux-ipv6.be>
2023-12-06 20:45:44 +0200

    migrations: import metadata for ConfigTable
    This makes it possible to auto-generate the db migrations from the
    SQLModel models.

--- migrations/env.py
+++ migrations/env.py
@@ -2,8 +2,12 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlmodel import SQLModel
 
 from alembic import context
+
+from app.db.models import WillowClientTable
+from app.db.models import WillowConfigTable
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
