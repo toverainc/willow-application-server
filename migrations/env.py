c339f5066edbbd7235ffee6a1b78c22a67dbb257
Stijn Tintel <stijn@linux-ipv6.be>
2023-12-07 23:41:20 +0200

    migrations: use DB_URL from app.const
    This is in preparation to allow overriding the DB_URL via an environment
    variable.

--- migrations/env.py
+++ migrations/env.py
@@ -6,12 +6,15 @@ from sqlmodel import SQLModel
 
 from alembic import context
 
+from app.const import DB_URL
 from app.db.models import WillowClientTable
 from app.db.models import WillowConfigTable
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option('sqlalchemy.url', DB_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
