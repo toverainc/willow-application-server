02a26e46484c6d7de1c8bf017039dd7deaef354e
Stijn Tintel <stijn@linux-ipv6.be>
2024-01-10 13:57:15 +0200

    settings: add db_url

--- app/settings.py
+++ app/settings.py
@@ -2,8 +2,11 @@ from functools import lru_cache
 
 from pydantic_settings import BaseSettings
 
+from app.const import DB_URL
+
 
 class Settings(BaseSettings):
+    db_url: str = DB_URL
     was_version: str = "unknown"
 
 
