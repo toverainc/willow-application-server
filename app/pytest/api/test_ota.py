be7bd9ded1dfeb1d09b002c02e1b75a1454ade78
Stijn Tintel <stijn@linux-ipv6.be>
2025-04-28 14:41:05 +0300

    pytest: drop __init__ class method from TestOta
    As we now create the static/admin dir in a pytest fixture, we no longer
    need this.

--- app/pytest/api/test_ota.py
+++ app/pytest/api/test_ota.py
@@ -5,14 +5,12 @@ from unittest.mock import MagicMock, patch
 
 from fastapi.testclient import TestClient
 
+from app.main import app
+
+client = TestClient(app)
+
 
 class TestOta(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestOta, self).__init__(*args, **kwargs)
-
-        from app.main import app
-        self.client = TestClient(app)
-
     def test_get_ota(self):
         mock_uri_bad = "/api/ota?platform=ESP32-S3-BOX-3&version=0.0.0-mock.0/../../.."
         mock_uri_good = "/api/ota?platform=ESP32-S3-BOX-3&version=0.0.0-mock.0"
@@ -34,18 +32,18 @@ class TestOta(unittest.TestCase):
             # this will write mock_response to storage/ota/0.0.0-mock.0/ESP32-S3-BOX-3.bin
             with patch("os.path.isfile", return_value=False):
                 with patch("app.routers.ota.get", return_value=mock_response):
-                    response = self.client.get(mock_uri_good)
+                    response = client.get(mock_uri_good)
 
                     assert response.status_code == 404
 
             # os.path.isfile is not patched here so we serve the file content
-            response = self.client.get(mock_uri_good)
+            response = client.get(mock_uri_good)
 
             assert response.status_code == 200
             assert response.content == b"mocked data"
 
             # use bad URL that tries to read data outside OTA_DIR
-            response = self.client.get(mock_uri_bad)
+            response = client.get(mock_uri_bad)
 
             json_response = json.loads(response.content)
             print(json_response)
