f6660be2dabc1816e40bee83eb159ba433026c61
Stijn Tintel <stijn@linux-ipv6.be>
2025-04-28 14:28:43 +0300

    pytest: add test for GET /api/ota
    While fixing CodeQL reports, I completely missed the fact that
    is_safe_path returns a boolean, and not an actual path, and broke
    several API endpoints.
    
    This could have been avoided by having a test, so add a test for the GET
    /api/ota endpoint. The test might look ugly and hacky, but it would have
    caught the breakage I introduced. Better to have an ugly test than no
    test at all.
    
    Suggestions to make things cleaner are always welcome.

---
+++ app/pytest/api/test_ota.py
@@ -0,0 +1,62 @@
+import json
+import unittest
+
+from os import makedirs
+from unittest.mock import MagicMock, patch
+
+from fastapi.testclient import TestClient
+
+
+class TestOta(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestOta, self).__init__(*args, **kwargs)
+
+        # make sure the static/admin dir exists so we can run tests without having to copy the UI
+        makedirs("/app/static/admin", exist_ok=True)
+
+        from app.main import app
+        self.client = TestClient(app)
+
+    def test_get_ota(self):
+        mock_uri_bad = "/api/ota?platform=ESP32-S3-BOX-3&version=0.0.0-mock.0/../../.."
+        mock_uri_good = "/api/ota?platform=ESP32-S3-BOX-3&version=0.0.0-mock.0"
+        mock_releases_willow = [{
+            "name": "0.0.0-mock.0",
+            "tag_name": "0.0.0-mock.0",
+            "assets": [
+                {
+                    "browser_download_url": "bogus",
+                    "platform": "ESP32-S3-BOX-3",
+                }
+            ]
+        }]
+        mock_response = MagicMock()
+        mock_response.content = b"mocked data"
+
+        with patch("app.routers.ota.get_releases_willow", return_value=mock_releases_willow):
+            # patch os.path.isfile so we call get_releases_willow()
+            # this will write mock_response to storage/ota/0.0.0-mock.0/ESP32-S3-BOX-3.bin
+            with patch("os.path.isfile", return_value=False):
+                with patch("app.routers.ota.get", return_value=mock_response):
+                    response = self.client.get(mock_uri_good)
+
+                    assert response.status_code == 404
+
+            # os.path.isfile is not patched here so we serve the file content
+            response = self.client.get(mock_uri_good)
+
+            assert response.status_code == 200
+            assert response.content == b"mocked data"
+
+            # use bad URL that tries to read data outside OTA_DIR
+            response = self.client.get(mock_uri_bad)
+
+            json_response = json.loads(response.content)
+            print(json_response)
+
+            assert response.status_code == 400
+            assert json_response['detail'].startswith("invalid asset path")
+
+
+if __name__ == '__main__':
+    unittest.main()
